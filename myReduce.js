2c9e09f6b38778163b5059974fdc85ff365be199
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2023-02-26 14:08:39 +0700

    Update myReduce.js

--- myReduce.js
+++ myReduce.js
@@ -1,15 +1,13 @@
 Array.prototype.myReduce = function (callback) {
-    let accumulator = this[0];//
-    for (let i = 1; i < this.length; i++) {
-        accumulator += callback(accumulator, this[i]);
-        console.log(accumulator);
-        return callback(accumulator, this[i]);
+    let accumulator = this[0];
+    for (let i = 1; i<this.length; i++) {
+        accumulator = callback(accumulator, this[i]);
     }
-    
+    return accumulator;    
 }
-// callback (result, item)
-var array = [1,2,3];
+var array = [1,2,3,4,5];
 let newArray = array.myReduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
+    return accumulator * currentValue;
 });
 console.log(newArray);
+
