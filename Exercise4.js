740e6c0e8cff13be084968aeb3eef329b6b65433
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2023-02-27 17:33:53 +0700

    Update Exercise4.js

--- Exercise4.js
+++ Exercise4.js
@@ -1,13 +1,20 @@
+/** *******Requirement
+4. Get a new array from the given array so that the elements appear only once (implement 2 ways: using Set, not using Set)
+Example: [1,2,3,1,2,3,4,5,6,4] => [1,2,3,4,5,6]
+*/
+
+/** Program */
 // With Set
-var array = [1,2,3,1,2,3,4,5,6,4];
-const remove1 = [...new Set(array)];
-console.log(remove1);
+const array = [1, 2, 3, 1, 2, 3, 4, 5, 6, 4];
+const remove = [...new Set(array)];
 
 // Without Set
-const remove2 = [];
-array.forEach(element => {
-    if (!remove2.includes(element)) {
-        remove2.push(element);
-    }
-});
-console.log(remove2);
+const result = [];
+const map = {};
+for (let i = 0; i < array.length; i++) {
+  if (map[array[i]] === undefined) {
+    result.push(array[i]);
+    map[array[i]] = true;
+  }
+}
+console.log(result);
