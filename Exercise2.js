7ed5993ea8af85085ce329eae53d29d60b83eb67
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2023-02-27 16:21:51 +0700

    Update Exercise2.js

--- Exercise2.js
+++ Exercise2.js
@@ -1,4 +1,10 @@
-var array = [1,7,9,2,5,3,8];
-var n = array.length;
+/** *******Requirement
+2. Find 2 elements with the greatest sum in an array
+Example: [1,7,9,2,5,3,8] => [9,8] 
+*/
+
+/** Program */
+const array = [1, 7, 9, 2, 5, 3, 8];
+const n = array.length;
 array.sort((a, b) => a - b);
-console.log([array[n-1], array[n-2]]);
+console.log([array[n - 1], array[n - 2]]);
