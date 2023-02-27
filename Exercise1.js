5ff3f0f15b8b24b831161b6098a02941e5c1558f
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2023-02-27 10:55:35 +0700

    Update Exercise1.js

--- Exercise1.js
+++ Exercise1.js
@@ -1,15 +1,23 @@
-function getDuplicate(arr1, arr2) {
-    let result = [];
-    for (let i=0; i<arr1.length; i++) {
-        for (let j=0; j<arr2.length; j++) {
-            if (arr2[j] === arr1[i]) {
-                result.push(arr2[j]);
-                break;
-            }
-        }
+/**Write a function with 2 input parameters (each parameter is an array). This function returns an array of the same elements in two arrays;	
+
+Example: 	array1 = [1,2,3,4,5,6];
+			array2 = [3,4,8,9,12];
+			=> [3,4] */
+const array1 = [1, 2, 3, 4, 5, 6];
+const array2 = [3, 4, 8, 9, 12];
+const duplicates = [];
+const uniqueElements = {};
+function getDuplicate(array1, array2) {
+  array1.forEach((item) => {
+    if (!uniqueElements[item]) {
+      uniqueElements[item] = true;
     }
-    return result;
+  });
+  array2.forEach((item) => {
+    if (uniqueElements[item]) {
+      duplicates.push(item);
+    }
+  });
+  return duplicates;
 }
-array1 = [1,2,3,4,5,6];
-array2 = [3,4,8,9,12];
 console.log(getDuplicate(array1, array2));
