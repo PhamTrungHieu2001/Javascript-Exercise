42e05f68cc40fd95a15a0cb92ad2ebd34e07788b
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2023-02-27 16:16:33 +0700

    Update Exercise1.js

--- Exercise1.js
+++ Exercise1.js
@@ -1,4 +1,5 @@
-/**Write a function with 2 input parameters (each parameter is an array). This function returns an array of the same elements in two arrays;	
+/** *******Requirement
+1. Write a function with 2 input parameters (each parameter is an array). This function returns an array of the same elements in two arrays;	
 
 Example: 	array1 = [1,2,3,4,5,6];
 			array2 = [3,4,8,9,12];
