1bc523b964ca4e6887af6647b33c9e12017642a4
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2023-02-27 17:46:19 +0700

    Update Exercise5.js

--- Exercise5.js
+++ Exercise5.js
@@ -1,16 +1,21 @@
+/** *******Requirement
+5. Get a new array of duplicate elements in 2 arrays, each element appears exactly once in the new array
+Example: 	const arr1 = [1, 2, 3, 4, 5, 6, 7, 9, 9, 8, 7, 7];
+			const arr2 = [3, 5, 9, 10, 88];
+			=> [3,5,9]
+*/
+
+/** Program */
 const arr1 = [1, 2, 3, 4, 5, 6, 7, 9, 9, 8, 7, 7];
 const arr2 = [3, 5, 9, 10, 88];
-// remove all duplicates in each array
-const remove1 = arr1.filter((element, index) => arr1.indexOf(element) == index);
-const remove2 = arr2.filter((element, index) => arr2.indexOf(element) == index);
-// find duplicates
-function getDuplicate(arr1, arr2) {
-    const result = [];
-    for (let i=0; i<arr1.length; i++) {
-        if (arr2.includes(arr1[i])) {
-            result.push(arr1[i]);
-        }           
-    }
-    return result;
+const map = {};
+const result = [];
+for (let i = 0; i < arr1.length; i++) {
+  if (map[arr1[i]] === undefined) {
+    map[arr1[i]] = true;
+  }
 }
-console.log(getDuplicate(remove1, remove2));
+for (let i = 0; i < arr2.length; i++) {
+  if (map[arr2[i]]) result.push(arr2[i]);
+}
+console.log(result);
