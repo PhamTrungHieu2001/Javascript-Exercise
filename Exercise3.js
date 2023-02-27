ed4ca6b63ab7340d5c2019ea36ec88c8b728f9ab
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2023-02-27 16:48:41 +0700

    Update Exercise3.js

--- Exercise3.js
+++ Exercise3.js
@@ -1,13 +1,17 @@
+/** *******Requirement
+3. Find pairs of elements whose sum is equal to a given number
+Example: [1,7,9,2,5,3,8], sum = 10 => [1,9] ; [7,3]; [2,8]
+*/
+
+/** Program */
 const sum = 10;
-const array = [1,7,9,2,5,3,8];
+const array = [1, 7, 9, 2, 5, 3, 8];
+const pairs = {};
 const result = [];
 const n = array.length;
-for (let i = 0; i < n-1; i++) {
-    for (let j = i+1; j < n; j++) {
-        if (array[i] + array[j] == sum) {
-            result.push([array[i], array[j]]);
-            break;
-        }
-    }
+for (let i = 0; i < n; i++) {
+  if (pairs[array[i]] === undefined) {
+    pairs[sum - array[i]] = array[i];
+  } else result.push([pairs[array[i]], array[i]]);
 }
 console.log(result);
